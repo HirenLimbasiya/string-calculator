9ade6f3bec5281d56d033bbfc7001f59453c3766
Hiren <hiren@Hirens-MacBook-Air.local>
2024-10-16 22:25:33 +0530

    add comments in addFunction

--- calculator.js
+++ calculator.js
@@ -2,24 +2,25 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  let delimiter = /,|\n/;
+  let delimiter = /,|\n/; // Default delimiters are comma and newline
 
+  // Check for custom delimiter syntax
   if (numbers.startsWith("//")) {
     const delimiterEndIndex = numbers.indexOf("\n");
     delimiter = new RegExp(numbers.slice(2, delimiterEndIndex));
-    numbers = numbers.substring(delimiterEndIndex + 1);
+    numbers = numbers.substring(delimiterEndIndex + 1); 
   }
 
   const numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
 
-  const negativeNumbers = numArray.filter((num) => num < 0);
+  const negativeNumbers = numArray.filter((num) => num < 0); 
   if (negativeNumbers.length > 0) {
     throw new Error(
       `Negative numbers not allowed: ${negativeNumbers.join(",")}`
-    );
+    ); // Throw error if any negative numbers are found
   }
 
-  return numArray.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
+  return numArray.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0); // Sum the numbers and return
 }
 
 module.exports = { add };
