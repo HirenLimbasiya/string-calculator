f7fe3d1bfe846bdce101c3c6eed20b032dd88010
Hiren <hiren@Hirens-MacBook-Air.local>
2024-10-16 21:46:11 +0530

    add all testcaase like new lines, diffrent delimiter, single or multi negative mumber

--- calculator.test.js
+++ calculator.test.js
@@ -16,4 +16,22 @@ describe("String Calculator", () => {
   test("should handle any amount of numbers", () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
+
+  test("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+
+  test("should support different delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+
+  test("should throw an error for negative numbers", () => {
+    expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+  });
+
+  test("should throw an error listing all negative numbers", () => {
+    expect(() => add("1,-2,-3,4")).toThrow(
+      "Negative numbers not allowed: -2,-3"
+    );
+  });
 });
